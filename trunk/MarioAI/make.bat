925ee60729456a4b6a998e94579411ba83b0ee23
Johnny31525@gmail.com <Johnny31525@gmail.com@8c5015f3-a2cc-8386-8335-b7f65816da76>
2012-06-15 11:16:47 +0000

    update windows makefile

--- trunk/MarioAI/make.bat
+++ trunk/MarioAI/make.bat
@@ -1,4 +1,5 @@
 mkdir bin
 dir src\*.java /s /b > sources_list.txt
 javac -d bin -encoding latin1 -classpath lib\asm-all-3.3.jar;lib\jdom.jar;lib\junit-4.8.2.jar;.;src @sources_list.txt
-copy resources bin\ch\idsia\benchmark\mario\engine
+mkdir bin\ch\idsia\benchmark\mario\engine\resources
+copy resources bin\ch\idsia\benchmark\mario\engine\resources
