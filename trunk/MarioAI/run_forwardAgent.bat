5aa31adae766f14356eb476b6a01b1112f1f4c39
Johnny31525@gmail.com <Johnny31525@gmail.com@8c5015f3-a2cc-8386-8335-b7f65816da76>
2012-06-17 05:08:21 +0000

    ChungYiParallelTask finished!!!!

--- trunk/MarioAI/run_forwardAgent.bat
+++ trunk/MarioAI/run_forwardAgent.bat
@@ -1,1 +1,1 @@
-java -Xmx1000m -classpath lib\asm-all-3.3.jar;lib\jdom.jar;lib\junit-4.8.2.jar;bin ch.idsia.scenarios.Main -ag ch.idsia.agents.controllers.ForwardAgent -ld 1 -task Replay
+java -Xmx1000m -classpath lib\asm-all-3.3.jar;lib\jdom.jar;lib\junit-4.8.2.jar;bin ch.idsia.scenarios.Main -ag ch.idsia.agents.controllers.ForwardAgent -ld 2 -task ChungYiParallel
