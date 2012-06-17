18072be14760b36a38bed5b6bcf6b04ad109c218
ianchou821 <ianchou821@8c5015f3-a2cc-8386-8335-b7f65816da76>
2012-06-17 02:58:38 +0000

    fixed abnormal evaluation info

--- MarioAI/src/ch/idsia/benchmark/mario/engine/sprites/Enemy.java
+++ MarioAI/src/ch/idsia/benchmark/mario/engine/sprites/Enemy.java
@@ -158,12 +158,12 @@ public void collideCheck()
                     {
                         spriteContext.addSprite(new Shell(levelScene, x, y, 1));
                     }
-                    ++LevelScene.killedCreaturesTotal;
-                    ++LevelScene.killedCreaturesByStomp;
+                    ++levelScene.killedCreaturesTotal;
+                    ++levelScene.killedCreaturesByStomp;
                 }
             } else
             {
-                levelScene.mario.getHurt(this.kind);
+            	levelScene.mario.getHurt(this.kind);
             }
         }
     }
@@ -377,8 +377,8 @@ public boolean shellCollideCheck(Shell shell)
             hPic = -hPic;
             yPicO = -yPicO + 16;
 //                System.out.println("shellCollideCheck");
-            ++LevelScene.killedCreaturesTotal;
-            ++LevelScene.killedCreaturesByShell;
+            ++levelScene.killedCreaturesTotal;
+            ++levelScene.killedCreaturesByShell;
             return true;
         }
     }
@@ -407,8 +407,8 @@ public boolean fireballCollideCheck(Fireball fireball)
             hPic = -hPic;
             yPicO = -yPicO + 16;
 //                System.out.println("fireballCollideCheck");
-            ++LevelScene.killedCreaturesTotal;
-            ++LevelScene.killedCreaturesByFireBall;
+            ++levelScene.killedCreaturesTotal;
+            ++levelScene.killedCreaturesByFireBall;
             return true;
         }
     }
